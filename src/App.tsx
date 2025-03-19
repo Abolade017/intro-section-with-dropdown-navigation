e4dd80d1e777fde583ed4de97b816635e4bd7387
Adeola Tawakaltu Adenike <tawakalituadeola68@gmail.com>
2025-03-19 13:14:36 +0100

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="font-bold ">hello world!</h1>
+      <p className="text-red-500 font-medium">hellooooo</p>
+    </>
+  );
+}
+
+export default App;
